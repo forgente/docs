532b5504e571d4d4bb0ebe96d384b6ddadecfad4
Lunny Xiao <xiaolunwen@gmail.com>
2023-12-13 18:24:51 +0800

    Upgrade to 1.21.2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -108,7 +108,7 @@ const config = {
               banner: 'unreleased',
             },
             '1.21': {
-              label: '1.21.1',
+              label: '1.21.2',
             },
             '1.20': {
               label: '1.20.6'
@@ -198,7 +198,7 @@ const config = {
             position: 'right',
             items: [
               {to: '/api/next/', label: '1.22-dev' },
-              {to: '/api/1.21/', label: '1.21.1' },
+              {to: '/api/1.21/', label: '1.21.2' },
               {to: '/api/1.20/', label: '1.20.6' },
               {to: '/api/1.19/', label: '1.19.4' },
             ],
